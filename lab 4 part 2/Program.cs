dc03d89aa5045c8e69f7c26566992ab25dd6e2f9
P4lerm0 <sasaermakov77@gmail.com>
2021-12-01 02:20:33 +0300

    Добавьте файлы проекта.

---
+++ lab 4 part 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab_4_part_2
+{
+    class Program
+    {
+
+        static double Method(double k)
+        {
+            double m = (k + 5) / 3;
+            return m;
+        }
+        static double Rec(double k)
+        {
+            if (k == -5)
+            {
+                return Method (k);
+            }
+            else
+            {
+                return Method(k) + Rec(k - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double m = Rec(n);
+
+            Console.WriteLine(m);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ part 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab_4_part_3
+{
+    class Program
+    {
+        static double Method(double x)
+        {
+            double a = (1 + x) / 2; //при постановке -1 всегда получается ответ 0
+            return a;
+        }
+        static double Rec(double k)
+        {
+            if (k == -1)
+                return 1;
+            else
+                return Method(k) * Rec(k - 1);
+
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double s = Rec(n);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
